09eb89b17cc7e98a4ce81952d47113932bbfe150
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-21 15:27:52 -0500

    🏗️ Adding modules for lazy loading

---
+++ src/app/games/games.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GamesRoutingModule } from './games-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GamesRoutingModule
+  ]
+})
+export class GamesModule { }
