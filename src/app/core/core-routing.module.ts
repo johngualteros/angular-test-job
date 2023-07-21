7971443e729568651c67bc61539f6bd82da9d03d
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-21 15:58:11 -0500

    ✨ Adding modules and home page

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { 
+  {
     path: '',
-    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+    component : HomeComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class CoreRoutingModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreRoutingModule } from './core-routing.module';
+
+/**
+ * Components layout
+ */
+import { HeaderComponent } from './layout/header/header.component';
+
+/**
+ * Pages
+ * @description
+ * Home page
+ * @see HomeComponent
+*/
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule
   ]
 })
 export class CoreModule { }
