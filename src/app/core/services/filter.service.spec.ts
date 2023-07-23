6ad2fcd98630178d14594b5b8bc7c5a8d248c9d5
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-23 08:53:47 -0500

    ✅ Completing the core tests all passed

--- src/app/core/services/filter.service.spec.ts
+++ src/app/core/services/filter.service.spec.ts
@@ -13,4 +13,18 @@ describe('FilterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set platform filter', () => {
+    service.setPlatformFilter('platform');
+    service.platformFilter$.subscribe((platform) => {
+      expect(platform).toBe('platform');
+    });
+  });
+
+  it('should set genre filter', () => {
+    service.setGenreFilter('genre');
+    service.genreFilter$.subscribe((genre) => {
+      expect(genre).toBe('genre');
+    });
+  });
 });
--- src/app/core/services/games.service.spec.ts
+++ src/app/core/services/games.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GamesService } from './games.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment.development';
 
 describe('GamesService', () => {
+  let httpMock: HttpTestingController;
   let service: GamesService;
+  let apiURL = environment.apiURL;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(GamesService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have array of games', () => {
+    expect(service.games).toBeTruthy();
+  });
+
+  it('should have method getFilteredGames', () => {
+    expect(service.getFilteredGames).toBeTruthy();
+  });
+
+  it('should have method getGame', () => {
+    expect(service.getGame).toBeTruthy();
+  });
+
+  it('should get all games', () => {
+    service.getFilteredGames().subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+    const mockRequest = httpMock.expectOne(`${apiURL}/games`);
+    expect(mockRequest.request.method).toBe('GET');
+  });
+
+  it('should get a game by id', () => {
+    service.getGame(2).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+    const mockRequest = httpMock.expectOne(`${apiURL}/game?id=2`);
+    expect(mockRequest.request.method).toBe('GET');
   });
 });
