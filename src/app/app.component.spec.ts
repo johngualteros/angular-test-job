d2350b0c94e6d558143fd082f45b10995981a731
John Gualteros <jagualterosg@kumpels.com.co>
2023-07-23 11:18:42 -0500

    ✅ Adding all test passed, 88 tests executed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ThemesService } from './core/design/services/themes.service';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { NotifierModule } from 'angular-notifier';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, NotifierModule],
+    declarations: [AppComponent, LoaderComponent],
+    providers: [ThemesService]
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should appear loader', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const loaderComponent = fixture.debugElement.query(
+      By.directive(LoaderComponent)
+    );
+    expect(loaderComponent).toBeTruthy();
   });
 });
